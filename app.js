e9a5b80d5e345e773cf7c6413a3fff5a830855f5
w8xcy79ys6-beep <w8xcy79ys6@privaterelay.appleid.com>
2026-05-05 10:53:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
+function toHiragana(text) {
+  return text.replace(/[ァ-ン]/g, ch =>
+    String.fromCharCode(ch.charCodeAt(0) - 0x60)
+  );
+}
+
+function is575(text) {
+  const hira = toHiragana(text).replace(/[^ぁ-ん]/g, '');
+  
+  if (hira.length !== 17) return false;
+
+  const first = hira.slice(0, 5);
+  const second = hira.slice(5, 12);
+  const third = hira.slice(12, 17);
+
+  return first.length === 5 && second.length === 7 && third.length === 5;
+}
 app.use(express.json());
 
 const CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN;
@@ -15,7 +32,18 @@ app.post('/webhook', async (req, res) => {
 
     const message = {
       type: "text",
-      text: "受信：" + userText
+     let replyText;
+
+if (is575(userText)) {
+  replyText = "五七五！ナイス川柳👍";
+} else {
+  replyText = "五七五じゃないかも🤔";
+}
+
+const message = {
+  type: "text",
+  text: replyText
+};
     };
 
     await axios.post(
