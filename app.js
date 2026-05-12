65e3cd7492e024954ac10f41c2d095afcaac0848
w8xcy79ys6-beep <w8xcy79ys6@privaterelay.appleid.com>
2026-05-12 20:17:28 +0900

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let userOwnedTitles = {};
 const userNames = {};
 
 const ADMIN_IDS = [
-  "Ue7d9cad9eca94d92d52195c4ecf740d8"
+  "Ue7d9cad9eca94d92d52195c4ecf740d8","Uf0cdb8a90a7197ee95813e69e654f816"
 ];
 const titles = [
   { name: "見習い冒険者", rarity: "N", chance: 40 },
